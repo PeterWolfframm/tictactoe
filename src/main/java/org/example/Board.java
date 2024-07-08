69436aa1ef7505903ed20e5e7d23574a21267955
Ollon1 <wi23b121@technikum-wien.at>
2024-07-08 19:21:11 +0200

    board created

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class Board {
+
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j]);
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (i < 2) {
+                System.out.println("-----");
+            }
+        }
+    }
 }
--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class TicTacToe {
+
+    private Board board;
+
+    public TicTacToe(){
+        board = new Board();
+    }
+
+    public void start() {
+        board.clear();
+        boolean gameOn = true;
+
+        while (gameOn) {
+            board.print();
+            gameOn = false;
+
+        }
+    }
+
 }
