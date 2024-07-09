89b560ffe6eb82aed79c408d59430a78a694904f
PeterWolfframm <servus@peterwolfframm.com>
2024-07-09 20:21:46 +0200

    wrote the tests

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -48,15 +48,15 @@ public class TicTacToe {
         System.out.print(message);
         return new java.util.Scanner(System.in).nextInt();
     }
-    private void switchCurrentPlayer() {
+    void switchCurrentPlayer() {
         currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
-    private boolean hasWinner() {
+    boolean hasWinner() {
         char position = currentPlayer.getZeichen();
         return (checkRows(position) || checkColumns(position) || checkDiagonals(position));
     }
 
-    private boolean checkRows(char position) {
+    boolean checkRows(char position) {
         for (int i = 0; i < 3; i++) {
             if (!board.isCellEmpty(i, 0) && !board.isCellEmpty(i, 1) && !board.isCellEmpty(i, 2)
                     && board.getCell(i, 0) == position && board.getCell(i, 1) == position && board.getCell(i, 2) == position) {
@@ -66,7 +66,7 @@ public class TicTacToe {
         return false;
     }
 
-    private boolean checkColumns(char position) {
+    boolean checkColumns(char position) {
         for (int i = 0; i < 3; i++) {
             if (!board.isCellEmpty(0, i) && !board.isCellEmpty(1, i) && !board.isCellEmpty(2, i)
                     && board.getCell(0, i) == position && board.getCell(1, i) == position && board.getCell(2, i) == position) {
@@ -76,10 +76,22 @@ public class TicTacToe {
         return false;
     }
 
-    private boolean checkDiagonals(char zeichen) {
+    boolean checkDiagonals(char zeichen) {
         return (!board.isCellEmpty(0, 0) && !board.isCellEmpty(1, 1) && !board.isCellEmpty(2, 2)
                 && board.getCell(0, 0) == zeichen && board.getCell(1, 1) == zeichen && board.getCell(2, 2) == zeichen)
                 || (!board.isCellEmpty(0, 2) && !board.isCellEmpty(1, 1) && !board.isCellEmpty(2, 0)
                 && board.getCell(0, 2) == zeichen && board.getCell(1, 1) == zeichen && board.getCell(2, 0) == zeichen);
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public void setCurrentPlayer(Player player) {
+        this.currentPlayer = player;
+    }
+
+    public Player getCurrentPlayer() {
+        return this.currentPlayer;
     }
 }
