552a44217fb356f7e411ab687467b530feb694b3
PeterWolfframm <servus@peterwolfframm.com>
2024-07-09 22:41:15 +0200

    for some reason, the tests stopped working, fixed it

--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Tests {
+public class TicTacToeTest {
 
     private TicTacToe ticTacToe;
     private Board board;
